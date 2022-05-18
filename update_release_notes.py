c32e8dd77f093809c5f5be87708dd6a4e1e2cf3d
Leandro Palazzolo <leandro@blockwallet.io>
2022-05-18 17:17:17 -0300

    Add script

---
+++ update_release_notes.py
@@ -0,0 +1,23 @@
+import os
+import json
+import sys
+
+json_data={}
+
+version = os.getenv("VERSION")
+file = os.getenv("FILE")
+data = os.getenv("DATA")
+
+with open(file) as json_file:
+    json_data = json.load(json_file)
+
+new_data = json.loads(data)
+new_data["version"] = version
+for note in json_data['releaseNotes']:
+    if note["version"] == version:
+        print(f"Existing release note for version {version}. Aborting")
+        sys.exit(1)
+json_data['releaseNotes'].append(new_data)
+
+with open(os.getenv("FILE"), "w") as updated_json_file: 
+    json.dump(json_data, updated_json_file, indent=2)
