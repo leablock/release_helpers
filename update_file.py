b6243f68f6f3fa5e4d1b5ce8808179051195533a
Leandro Palazzolo <leandro@blockwallet.io>
2022-05-19 13:32:47 -0300

    Update

--- update_file.py
+++ update_file.py
@@ -11,14 +11,16 @@ data = os.getenv("DATA")
 with open(file) as json_file:
     json_data = json.load(json_file)
 
-new_data = json.loads(data)
-new_data["version"] = version
 if data is not None:
+    new_data = json.loads(data)
+    new_data["version"] = version
     for note in json_data['releaseNotes']:
         if note["version"] == version:
             print(f"Existing release note for version {version}. Aborting")
             sys.exit(1)
     json_data['releaseNotes'].append(new_data)
+else:
+    json_data["version"] = version
 
 with open(os.getenv("FILE"), "w") as updated_json_file: 
     json.dump(json_data, updated_json_file, indent=2)
